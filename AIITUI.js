b3bf8db060b5b9194b574a0e9597c7ae8310ffbe
Carlos Tang <tangyu_nju@163.com>
2021-09-26 20:21:56 +0800

    Add files via upload

---
+++ AIITUI.js
@@ -0,0 +1,17 @@
+/**
+ * 组件初始化
+ */
+(function(w){
+
+
+
+    let AIITUI = w.AIITUI || {};
+
+
+
+
+
+
+    w.AIITUI = AIITUI;
+
+})(window);
---
+++ index.js
@@ -0,0 +1,5 @@
+document.querySelector("#design").addEventListener("click", function () {
+
+    window.open("./design.html", "_self")
+
+});
